e3c7e03588b890e44c2175eb4c747b50b9196369
m34782 <lewis.hanly@microchip.com>
2021-01-08 13:31:55 +0000

    UIO Example applications updated
    Signed-off-by: Lewis Hanly <lewis.hanly@microchip.com>

--- recipes-tools/pfsoc_apps/files/LSRAM_read_write.c
+++ recipes-tools/pfsoc_apps/files/LSRAM_read_write.c
@@ -7,46 +7,126 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-#define UIO_0_DEVICE    "/dev/uio0"
+#define SYSFS_PATH_LEN		(128)
+#define ID_STR_LEN		(32)
+#define UIO_DEVICE_PATH_LEN	(32)
+#define NUM_UIO_DEVICES		(32)
 
-#define MMAP_SIZE     0x10000
+char uio_id_str[] = "uio_lpddr4";
+char sysfs_template[] = "/sys/class/uio/uio%d/%s";
+
+/*****************************************/
+/* function get_uio_device will return */
+/* the uio device number */
+/*************************************** */
+
+int get_uio_device(char * id)
+{
+	FILE *fp;
+	int i;
+	int len;
+	char file_id[ID_STR_LEN];
+	char sysfs_path[SYSFS_PATH_LEN];
+
+	for (i = 0; i < NUM_UIO_DEVICES; i++) {
+		snprintf(sysfs_path, SYSFS_PATH_LEN, sysfs_template, i, "/name");
+		fp = fopen(sysfs_path, "r");
+		if (fp == NULL)
+			break;
+
+		fscanf(fp, "%32s", file_id);
+		len = strlen(id);
+		if (len > ID_STR_LEN-1)
+			len = ID_STR_LEN-1;
+		if (strncmp(file_id, id, len) == 0) {
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+/*****************************************/
+/* function get_memory_size will return */
+/* the uio device size */
+/*************************************** */
+
+uint32_t get_memory_size(char *sysfs_path, char *uio_device)
+{
+	FILE *fp;
+	uint32_t sz;
+
+	/* 
+	 * open the file the describes the memory range size.
+	 * this is set up by the reg property of the node in the device tree
+	 */
+	fp = fopen(sysfs_path, "r");
+	if (fp == NULL) {
+		fprintf(stderr, "unable to determine size for %s\n", uio_device);
+		exit(0);
+	}
+
+	fscanf(fp, "0x%016X", &sz);
+	fclose(fp);
+	return sz;
+}
 
 int main(int argc, char* argvp[])
 {
 	int retCode = 0;
-	int uioFd_0,uioFd_1,d=0;
-	char d1;
-	volatile uint32_t* mem_ptr0,*mem_ptr1,*mem_ptr2,*mem_ptr3;
-	uint32_t memread[5]={0xAA,0x77,0xbb,0xcc,0x05};
+	int uioFd_0,index=0;
+	char uio_device[UIO_DEVICE_PATH_LEN];
+	char sysfs_path[SYSFS_PATH_LEN], d1;
+	volatile uint32_t *mem_ptr0;
+	uint32_t mmap_size;
 	uint32_t val = 0;
 	volatile uint32_t i =0 ;
 
- 
-	// Open uio_0 device
-	uioFd_0 = open(UIO_0_DEVICE, O_RDWR);
-	if(uioFd_0 < 0)
-	{
-		fprintf(stderr, "Cannot open %s: %s\n", UIO_0_DEVICE, strerror(errno));
+	printf("locating device for %s\n", uio_id_str);
+        index = get_uio_device(uio_id_str);
+	if (index < 0) {
+		fprintf(stderr, "can't locate uio device for %s\n", uio_id_str);
 		return -1;
 	}
-	while(1){
+
+	snprintf(uio_device, UIO_DEVICE_PATH_LEN, "/dev/uio%d", index);
+	printf("located %s\n", uio_device);
+
+	uioFd_0 = open(uio_device, O_RDWR);
+	if(uioFd_0 < 0) {
+		fprintf(stderr, "cannot open %s: %s\n", uio_device, strerror(errno));
+		return -1;
+	} else {
+		printf("opened %s (r,w)\n", uio_device);
+	}
+
+	snprintf(sysfs_path, SYSFS_PATH_LEN, sysfs_template, index, "maps/map0/size");
+	mmap_size = get_memory_size(sysfs_path, uio_device);
+	if (mmap_size == 0) {
+		fprintf(stderr, "bad memory size for %s\n", uio_device);
+		return -1;
+	}
+
+	while(1) {
 		printf("\n\t # Choose one of  the following options: \n\t Enter 1 to perform memory test on LSRAM \n\t Enter 2 to Exit\n");
 		scanf("%c%*c",&d1);
 		if(d1=='2'){
 			break;
 		}else if(d1=='1'){
-			mem_ptr0 = mmap(NULL, MMAP_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, uioFd_0, 0*getpagesize());
+			mem_ptr0 = mmap(NULL, mmap_size, PROT_READ|PROT_WRITE, MAP_SHARED, uioFd_0, 0*getpagesize());
 			if(mem_ptr0 == MAP_FAILED){
 				fprintf(stderr, "Cannot mmap: %s\n", strerror(errno));
 				close(uioFd_0);
 				return -1;
 			}
+			printf("\nSize of memory at 0x61000000 is 0x%x\n", mmap_size);
 			printf("\nWriting incremental pattern starting from address 0x61000000\n");
 			printf("\nReading data starting from address 0x61000000 \n");
 			printf("\nComparing data \n");
 
-			for(i=0;i<(MMAP_SIZE/4);i++) {
+			for(i=0;i<(mmap_size/4);i++) {
 				*(mem_ptr0+i)=i;
 				val=*(mem_ptr0+i);
                                 if(val !=i){
@@ -59,14 +139,16 @@ int main(int argc, char* argvp[])
                                 }
 
 			}
-			if(i == (MMAP_SIZE/4))
+			if(i == (mmap_size/4))
 				printf("\n\n\n**** LSRAM memory test passed successfully *****\n");
 		}else {
 			printf("Enter either 1 or 2\n");
 		}
-		munmap((void*)mem_ptr0, MMAP_SIZE);
+		retCode = munmap((void*)mem_ptr0, mmap_size);
+		printf("unmapped %s\n", uio_device);
 	}
 
-	close(uioFd_0);
+	retCode = close(uioFd_0);
+	printf("closed %s\n", uio_device);
 	return retCode;
 }
--- recipes-tools/pfsoc_apps/files/uio_dma_interrupt.c
+++ recipes-tools/pfsoc_apps/files/uio_dma_interrupt.c
@@ -9,51 +9,156 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <poll.h>
+#include <stdlib.h>
+
+#define SYSFS_PATH_LEN		(128)
+#define ID_STR_LEN		(32)
+#define UIO_DEVICE_PATH_LEN	(32)
+#define NUM_UIO_DEVICES		(32)
+
+char uio_id_str_lpddr4_lsram[] = "uio_lpddr4";
+char uio_id_str_dma_dev[] = "mss_dma0";
+char sysfs_template[] = "/sys/class/uio/uio%d/%s";
+
+/*****************************************/
+/* function get_uio_device will return */
+/* the uio device number */
+/*************************************** */
+
+int get_uio_device(char * id)
+{
+	FILE *fp;
+	int i;
+	int len;
+	char file_id[ID_STR_LEN];
+	char sysfs_path[SYSFS_PATH_LEN];
+
+	for (i = 0; i < NUM_UIO_DEVICES; i++) {
+		snprintf(sysfs_path, SYSFS_PATH_LEN, sysfs_template, i, "/name");
+		fp = fopen(sysfs_path, "r");
+		if (fp == NULL)
+			break;
+
+		fscanf(fp, "%32s", file_id);
+		len = strlen(id);
+		if (len > ID_STR_LEN-1)
+			len = ID_STR_LEN-1;
+		if (strncmp(file_id, id, len) == 0) {
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+/*****************************************/
+/* function get_memory_size will return */
+/* the uio device size */
+/*************************************** */
+
+uint32_t get_memory_size(char *sysfs_path, char *uio_device)
+{
+	FILE *fp;
+	uint32_t sz;
+
+	/* 
+	 * open the file the describes the memory range size.
+	 * this is set up by the reg property of the node in the device tree
+	 */
+	fp = fopen(sysfs_path, "r");
+	if (fp == NULL) {
+		fprintf(stderr, "unable to determine size for %s\n", uio_device);
+		exit(0);
+	}
+
+	fscanf(fp, "0x%016X", &sz);
+	fclose(fp);
+	return sz;
+}
 
 uint8_t dma_complete = 0;
 
-int uioFd_0,d=0;
-int uioFd_1;
-volatile uint32_t *ddr_mem,*dma_mem,*lsram_mem;
-
-#define UIO_0_DEVICE    "/dev/uio0"
-#define UIO_1_DEVICE    "/dev/uio2"
-
-#define MMAP_SIZE     0x10000
 
 int main(int argc, char* argvp[])
 {
-	int retCode = 0;
 	char d1;	
-	uint32_t memread[5]={0xAA,0x77,0xbb,0xcc,0x05};
 	uint32_t val = 0;
+	char uio_device_mem[UIO_DEVICE_PATH_LEN];
+	char uio_device_dma_dev[UIO_DEVICE_PATH_LEN];
+	char sysfs_path[SYSFS_PATH_LEN];
+	uint32_t mmap_size_lpddr4, mmap_size_lsram, mmap_size_dma_dev;
+	volatile uint32_t *lpddr4_mem,*dma_dev,*lsram_mem;
 	volatile uint32_t i=0 ;
+	int uioFd_0,uioFd_1, index=0;
 	uint32_t var1 =0;
 	int32_t intInfo;
 	ssize_t readSize;
-	uint32_t reenable=1,pending=0,temp1;
+	uint32_t pending=0;
+	//uint32_t temp1;
+
+	printf("locating device for %s\n", uio_id_str_lpddr4_lsram);
+        index = get_uio_device(uio_id_str_lpddr4_lsram);
+	if (index < 0) {
+		fprintf(stderr, "can't locate uio device for %s\n", uio_id_str_lpddr4_lsram);
+		return -1;
+	}
+
+	snprintf(uio_device_mem, UIO_DEVICE_PATH_LEN, "/dev/uio%d", index);
+	printf("located %s\n", uio_device_mem);
+
+	uioFd_0 = open(uio_device_mem, O_RDWR);
+	if(uioFd_0 < 0) {
+		fprintf(stderr, "cannot open %s: %s\n", uio_device_mem, strerror(errno));
+		return -1;
+	} else {
+		printf("opened %s (r,w)\n", uio_device_mem);
+	}
+
+	snprintf(sysfs_path, SYSFS_PATH_LEN, sysfs_template, index, "maps/map0/size");
+	mmap_size_lsram = get_memory_size(sysfs_path, uio_device_mem);
+	if (mmap_size_lsram == 0) {
+		fprintf(stderr, "bad memory size for %s\n", uio_device_mem);
+		return -1;
+	}
+
+	snprintf(sysfs_path, SYSFS_PATH_LEN, sysfs_template, index, "maps/map1/size");
+	mmap_size_lpddr4 = get_memory_size(sysfs_path, uio_device_mem);
+	if (mmap_size_lpddr4 == 0) {
+		fprintf(stderr, "bad memory size for %s\n", uio_device_mem);
+		return -1;
+	}
  
-	// Open uio_0 device
-	uioFd_0 = open(UIO_0_DEVICE, O_RDWR);
-	if(uioFd_0 < 0)
-	{
-		fprintf(stderr, "Cannot open %s: %s\n", UIO_0_DEVICE, strerror(errno));
+	printf("locating device for %s\n", uio_id_str_dma_dev);
+        index = get_uio_device(uio_id_str_dma_dev);
+	if (index < 0) {
+		fprintf(stderr, "can't locate uio device for %s\n", uio_id_str_dma_dev);
 		return -1;
 	}
 
-	uioFd_1 = open(UIO_1_DEVICE, O_RDWR);
-	if(uioFd_0 < 0)
-	{
-		fprintf(stderr, "Cannot open %s: %s\n", UIO_0_DEVICE, strerror(errno));
+	snprintf(uio_device_dma_dev, UIO_DEVICE_PATH_LEN, "/dev/uio%d", index);
+	printf("located %s\n", uio_device_dma_dev);
+
+	uioFd_1 = open(uio_device_dma_dev, O_RDWR);
+	if(uioFd_0 < 0) {
+		fprintf(stderr, "cannot open %s: %s\n", uio_device_dma_dev, strerror(errno));
+		return -1;
+	} else {
+		printf("opened %s (r,w)\n", uio_device_dma_dev);
+	}
+
+	snprintf(sysfs_path, SYSFS_PATH_LEN, sysfs_template, index, "maps/map0/size");
+	mmap_size_dma_dev = get_memory_size(sysfs_path, uio_device_dma_dev);
+	if (mmap_size_dma_dev == 0) {
+		fprintf(stderr, "bad memory size for %s\n", uio_device_dma_dev);
 		return -1;
 	}
 
-	dma_mem = mmap(NULL, 0x1000, PROT_READ|PROT_WRITE, MAP_SHARED, uioFd_1, 0*getpagesize());
-			if(dma_mem == MAP_FAILED){
-				fprintf(stderr, "Cannot mmap: %s\n", strerror(errno));
-				close(uioFd_0);
-				return -1;
-			}
+	dma_dev = mmap(NULL, mmap_size_dma_dev, PROT_READ|PROT_WRITE, MAP_SHARED, uioFd_1, 0*getpagesize());
+		if(dma_dev == MAP_FAILED){
+			fprintf(stderr, "Cannot mmap: %s\n", strerror(errno));
+			close(uioFd_1);
+			return -1;
+		}
 	
 	while(1){
 		printf("\n\t # Choose one of  the following options: \n\t Enter 1 to perform memory test on LSRAM \n\t Enter 2 to write data from LSRAM to LPDD4 via DMA access  \n\t Enter 3 to Exit\n");
@@ -62,7 +167,7 @@ int main(int argc, char* argvp[])
 			break;
 		}else if(d1=='1')
 			{
-				lsram_mem = mmap(NULL, MMAP_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, uioFd_0, 0*getpagesize());
+				lsram_mem = mmap(NULL, mmap_size_lsram, PROT_READ|PROT_WRITE, MAP_SHARED, uioFd_0, 0*getpagesize());
 				if(lsram_mem == MAP_FAILED){
 					fprintf(stderr, "Cannot mmap: %s\n", strerror(errno));
 					close(uioFd_0);
@@ -72,7 +177,7 @@ int main(int argc, char* argvp[])
 				printf("\nReading data starting from address 0x61000000 \n");
 				printf("\nComparing data \n");
 
-				for(i=0;i<(MMAP_SIZE/4);i++)
+				for(i=0;i<(mmap_size_lsram/4);i++)
 				{
 					*(lsram_mem+i)=i;
 				 	val=*(lsram_mem+i);
@@ -85,20 +190,20 @@ int main(int argc, char* argvp[])
                     				printf(".");
                     			}
 				}
-				if(i == (MMAP_SIZE/4))
+				if(i == (mmap_size_lsram/4))
 					printf("\n\n\n**** LSRAM memory test Passed with incremental pattern *****\n\n");
 		}
 		else if(d1=='2') 
 		{
-			lsram_mem = mmap(NULL, MMAP_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, uioFd_0, 0*getpagesize());
+			lsram_mem = mmap(NULL, mmap_size_lsram, PROT_READ|PROT_WRITE, MAP_SHARED, uioFd_0, 0*getpagesize());
 			if(lsram_mem == MAP_FAILED){
 				fprintf(stderr, "Cannot mmap: %s\n", strerror(errno));
 				close(uioFd_0);
 				return -1;
 			}
 			
-			ddr_mem = mmap(NULL, MMAP_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, uioFd_0, 1*getpagesize());
-			if(ddr_mem == MAP_FAILED){
+			lpddr4_mem = mmap(NULL, mmap_size_lpddr4, PROT_READ|PROT_WRITE, MAP_SHARED, uioFd_0, 1*getpagesize());
+			if(lpddr4_mem == MAP_FAILED){
 				fprintf(stderr, "Cannot mmap: %s\n", strerror(errno));
 				close(uioFd_0);
 				return -1;
@@ -116,30 +221,29 @@ int main(int argc, char* argvp[])
                     }
                }
 	    printf("\nFabric DMA controller configured for LSRAM to LPDDR4 data transfer.\n");
-            temp1 = *(dma_mem);   //version control register
+            //temp1 = *(dma_dev);   //version control register
 
-	    *(dma_mem + (0x14/4))  = 0x00000001;  //Interrupt mask
+	    *(dma_dev + (0x14/4))  = 0x00000001;  //Interrupt mask
 
-            *(dma_mem + (0x68/4))  = 0x61000000;  // Source address
+            *(dma_dev + (0x68/4))  = 0x61000000;  // Source address
             printf("\n\r\t Source Address (LSRAM) - 0x61000000 \n\r");
 
-            *(dma_mem + (0x6C/4))  = 0xC0000000;  //destination address
+            *(dma_dev + (0x6C/4))  = 0xC0000000;  //destination address
             printf("\n\r\tDestination Address (LPDDR4) - 0xC000000 \n\r");
 
 //            *(dma_mem + (0x64/4))  = 0x00000020;  //byte count
-            *(dma_mem + (0x64/4))  = 0x00010000;  //byte count
+            *(dma_dev + (0x64/4))  = 0x00010000;  //byte count
 	    printf("\n\r\tByte count - 0x10000 \n\r");
 
-	    *(dma_mem + (0x60/4))  = 0x0000F005;  //data ready,descriptor valid,sop,dop,chain
+	    *(dma_dev + (0x60/4))  = 0x0000F005;  //data ready,descriptor valid,sop,dop,chain
 
 	    printf("\n\rDMA Transfer Initiated... \n\r");
-            * (dma_mem + (0x04/4))  = 0x00000001;  //start operation register
+            * (dma_dev + (0x04/4))  = 0x00000001;  //start operation register
 
 	    readSize = read(uioFd_1, &pending, sizeof(intInfo));
             if(readSize < 0){
                    fprintf(stderr, "Cannot wait for uio device interrupt: %s\n",
                    strerror(errno));
-                   retCode = -1;
                    break;
                }
 			   
@@ -149,7 +253,7 @@ int main(int argc, char* argvp[])
 
 	    while(1){
                  for(i=0;i<8 ;i++){
-                     val = *(ddr_mem+i);
+                     val = *(lpddr4_mem+i);
                      var1 = *(lsram_mem+i);
                      if(val==var1){
                           printf(".");
@@ -174,13 +278,14 @@ int main(int argc, char* argvp[])
 	}else {
 			printf("Enter either 1, 2, and 3\n");
 		}
-	munmap((void*)lsram_mem, MMAP_SIZE);
-	munmap((void*)ddr_mem, MMAP_SIZE);
+	munmap((void*)lsram_mem, mmap_size_lsram);
+	munmap((void*)lpddr4_mem, mmap_size_lpddr4);
 
-	}	
-	munmap((void*)dma_mem, 0x1000);
+	}
+	munmap((void*)dma_dev, mmap_size_dma_dev);
 	close(uioFd_0);
+	printf("closed %s\n", uio_device_mem);
 	close(uioFd_1);
+	printf("closed %s\n", uio_device_dma_dev);
 	return 0;
 }
-
