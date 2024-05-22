ec32d1951d6e78abb5cca4ec700cbaa5667e8941
Ivan Griffin <ivan.griffin@microchip.com>
2024-05-22 09:56:58 +0100

    spdx: enable SPDX JSON generation by default
    Software Package Data Exchange (SPDX) is an open standard for software bill of
    materials (SBOM).
    
    A Software Bill of Materials (SBOM) is a comprehensive inventory that
    lists all the packages and libraries included in an application.
    
    SPDX captures versions of components, licenses, copyrights, security
    references and other metadata relating to software. SPDX is value in
    monitoring the security posture of a distribution. For more information,
    see https://spdx.dev or ISO/IEC 5692:2021.
    
    Signed-off-by: Ivan Griffin <ivan.griffin@microchip.com>

--- polarfire-soc_yocto_setup.sh
+++ polarfire-soc_yocto_setup.sh
@@ -104,6 +104,8 @@ require conf/distro/include/no-static-libs.inc
 require conf/distro/include/yocto-uninative.inc
 require conf/distro/include/security_flags.inc
 INHERIT += "uninative"
+INHERIT += "create-spdx"
+SPDX_PRETTY = "1"
 DISTRO_FEATURES:append = " largefile multiarch pam systemd "
 DISTRO_FEATURES_BACKFILL_CONSIDERED += "sysvinit"
 VIRTUAL-RUNTIME_init_manager = "systemd"
