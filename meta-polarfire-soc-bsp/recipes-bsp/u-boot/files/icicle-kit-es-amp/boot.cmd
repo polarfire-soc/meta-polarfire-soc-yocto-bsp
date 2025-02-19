5424bc9d20bb43c9789b5dee051a20e3bb09323e
Jamie Gibbons <jamie.gibbons@microchip.com>
2025-02-19 15:03:29 +0000

    u-boot: remove fdt set from all custom boot scripts
    With the latest U-Boot update, U-Boot now handles this step in the setup
    removing the need to set this from a custom boot script.
    
    Signed-off-by: Jamie Gibbons <jamie.gibbons@microchip.com>

--- meta-polarfire-soc-bsp/recipes-bsp/u-boot/files/icicle-kit-es-amp/boot.cmd
+++ meta-polarfire-soc-bsp/recipes-bsp/u-boot/files/icicle-kit-es-amp/boot.cmd
@@ -7,7 +7,5 @@ bootm loados ${scriptaddr};
 # Try to load a ramdisk if available inside fitImage
 bootm ramdisk;
 bootm prep;
-fdt set /soc/ethernet@20112000 mac-address ${icicle_mac_addr0};
-fdt set /soc/ethernet@20110000 mac-address ${icicle_mac_addr1};
 run design_overlays;
 bootm go;
--- meta-polarfire-soc-community/recipes-bsp/u-boot/files/beaglev-fire/boot.cmd
+++ meta-polarfire-soc-community/recipes-bsp/u-boot/files/beaglev-fire/boot.cmd
@@ -7,6 +7,5 @@ bootm loados ${scriptaddr};
 # Try to load a ramdisk if available inside fitImage
 bootm ramdisk;
 bootm prep;
-fdt set /soc/ethernet@20112000 mac-address ${beaglevfire_mac_addr0};
 run design_overlays;
 bootm go;
